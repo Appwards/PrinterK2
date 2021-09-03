30a5ce203667dce14235219fead6bebe59236aff
Jeremie Paas <jeremie.paas@gmail.com>
2021-09-03 15:00:56 +0400

    Implementation update

--- app/src/main/java/com/example/emptyprinter/MainActivity.java
+++ app/src/main/java/com/example/emptyprinter/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.emptyprinter;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
 import com.example.emptyprinter.aidlutils.AidlUtil;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AidlUtil.getInstance().connectPrinterService(this);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                int status = AidlUtil.getInstance().getPrinterStatus();
+                Log.i("Printer","Printer status: " + status);
+            }
+        }, 3000);
     }
 }
